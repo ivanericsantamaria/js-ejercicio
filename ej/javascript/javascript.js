39894f14b473d52f316b03e89efccbceaeaf52f8
Ivan Santamaria <ivanesantamaria@gmail.com>
2020-05-07 01:10:36 -0300

    jquery

--- ej/javascript/javascript.js
+++ ej/javascript/javascript.js
@@ -1,8 +1,19 @@
 
 function funcion () {
-    var input1= document.getElementById("input1").value;
-    document.getElementById("resultado1").textContent=input1; 
-   
+    
+    var input1= $("#valor").val();
+    // (javascript) var input1= document.getElementById("input1").value;
+    $("#resultado1").text(input1); 
+    // (javascript) document.getElementById("resultado1").textContent=input1;
+    
+    if (input1>1000) { //javascript
+        document.getElementById("resultado1").classList.add("text-success");
+        document.getElementById("resultado1").classList.remove("text-danger");
+    }else { //jquery
+        $("#resultado1").removeClass("text-success").addClass( "text-danger");
+        
+    }
+
 }
     
 
