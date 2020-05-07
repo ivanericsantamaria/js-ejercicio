b50c1bd2a748bbc9cbb67efaa44d5d76a84fa4aa
Ivan Santamaria <ivanesantamaria@gmail.com>
2020-05-07 02:31:00 -0300

    colores

--- ej/javascript/javascript.js
+++ ej/javascript/javascript.js
@@ -1,18 +1,23 @@
 
 function funcion () {
     
-    var input1= document.getElementById("valor").value;
-    // (javascript) var input1= document.getElementById("input1").value;
-    document.getElementById("resultado1").textContent=input1; 
-    // (javascript) document.getElementById("resultado1").textContent=input1;
+    var input1= document.getElementById("valor1").value;
     
-    if (input1>1000) { //javascript
-        document.getElementById("resultado1").classList.add("text-success");
-        document.getElementById("resultado1").classList.remove("text-danger");
-    }else { //jquery
+    document.getElementById("resultado1").textContent=input1; 
+    
+    
+    if (input1<1000) { 
         document.getElementById("resultado1").classList.add("text-danger");
         document.getElementById("resultado1").classList.remove("text-success");
-        
+        document.getElementById("resultado1").classList.remove("text-warning");
+    }else if (input1<3000){ 
+        document.getElementById("resultado1").classList.add("text-success");
+        document.getElementById("resultado1").classList.remove("text-danger");
+        document.getElementById("resultado1").classList.remove("text-warning");
+    }else if (input1>3000){
+        document.getElementById("resultado1").classList.add("text-warning");
+        document.getElementById("resultado1").classList.remove("text-success");
+        document.getElementById("resultado1").classList.remove("text-danger"); 
     }
 
 }
