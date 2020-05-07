5b8243693d0181ea53cf5a9f89bc2105ca4401c1
Ivan Santamaria <ivanesantamaria@gmail.com>
2020-05-07 02:07:48 -0300

    js nuevamente

--- ej/javascript/javascript.js
+++ ej/javascript/javascript.js
@@ -1,16 +1,17 @@
 
 function funcion () {
     
-    var input1= $("#valor").val();
+    var input1= document.getElementById("valor").value;
     // (javascript) var input1= document.getElementById("input1").value;
-    $("#resultado1").text(input1); 
+    document.getElementById("resultado1").textContent=input1; 
     // (javascript) document.getElementById("resultado1").textContent=input1;
     
     if (input1>1000) { //javascript
         document.getElementById("resultado1").classList.add("text-success");
         document.getElementById("resultado1").classList.remove("text-danger");
     }else { //jquery
-        $("#resultado1").removeClass("text-success").addClass( "text-danger");
+        document.getElementById("resultado1").classList.add("text-danger");
+        document.getElementById("resultado1").classList.remove("text-success");
         
     }
 
